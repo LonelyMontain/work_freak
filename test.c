1aaa4d6b833e553d39f64d2532d0ac7ea8a00401
LonelyMontain <ziyu.william.lu@outlook.com>
2019-10-28 01:00:25 -0400

    Add files via upload
    Fixed the output of the LList and a bug might count the same word as different node

--- test.c
+++ test.c
@@ -1,25 +1,27 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <stdio.h>
 #include "LList.h"
 
-char load[15];
-char buffer[20000];
+char load[200];
+char buffer[600000];
 
-char convert(char a)
+char convertToSmall(char a)
 {
     if(a >= 97)
         return a;
     return a + 32;
 }
 
+void resetarray(char * array, int length)
+{
+    for(int i = 0; i < length; i ++)
+    {
+        array[i] = ' ';
+    }
+}
+
 int strlenth(char *array)
 {
     int i = 0;
-    for(int j = 0; j < 15; j ++)
+    for(int j = 0; j < 5; j ++)
     {
         if(array[j] != ' ')
         {
@@ -29,35 +31,104 @@ int strlenth(char *array)
     return i;
 }
 
+char* convertToChar(int a, char *buffer)
+{
+    buffer = (char*)malloc(5*sizeof(char));
+    resetarray(buffer,5);
+    int copy = a;
+    while(a > 0)
+    {
+        if(a == 0)
+        {
+            break;
+        }
+        for(int i = 0; i < 5; i ++)
+        {
+            if(buffer[i] == ' ')
+            {
+                buffer[i] = copy % 10 + 48;
+                a /= 10;
+                copy = a;
+                i = 10;
+            }
+        }
+    }
+
+    char *result;
+    result = (char*)malloc(5*sizeof(char));
+    resetarray(result,5);
+
+    for (int i = 0; i < 5; i++)
+    {
+        if(buffer[i] == ' ')
+        {
+            continue;
+        }
+        result[4-i] = buffer[i];
+    }
+
+    int flag = -1;
+    for (int i = 0; i < 5; i++)
+    {
+        if(result[i] == ' ')
+        {
+            continue;
+        }
+        else
+        {
+            if(flag == -1)
+            {
+                flag = i;
+                buffer[i-flag] = result[i];
+            }
+            else
+            {
+                buffer[i-flag] = result[i];
+            }
+        }
+     }// buffer now is 200
+     char *r2;
+     r2 = (char*)malloc(5*sizeof(char));
+     resetarray(r2,5);
+     //printf("buffer len is: %i", strlenth(buffer));
+
+     int toleft = 5 - strlenth(buffer);
+     
+     if(toleft > 0)
+     {
+        for (int i = 0; i < strlenth(buffer); i++)
+        {
+            r2[toleft+i] = buffer[i];
+        }
+     }
+     else
+     {
+        return buffer;
+     }
+     return r2;
+}
+
+void readNode(struct Node *current)
+{
+    int count = current->count;
+    char *buffer;
+    char enter[] = {'\n'};
+    char is[] = {':'};
+    write(STDOUT_FILENO, current->name, 20);
+    write(STDOUT_FILENO, is, sizeof(is));
+    buffer = convertToChar(current->count, buffer);
+    write(STDOUT_FILENO, buffer, 5);
+    write(STDOUT_FILENO, enter, sizeof(enter));
+}
+
 void printLList(struct Node *head)
 {
     struct Node* current = head;
-    char buffer[5];
     while(current != NULL)
     {
-        write(STDOUT_FILENO, current->name, 15);
-        int check = sprintf(buffer, ", %i\n", current->count);
-        write(STDOUT_FILENO, buffer, 5);
+        readNode(current);
         current = current->next;
     }
-}
-
-void readstr(char * array, int length)
-{
-    for(int j = 0; j < length; j ++)
-    {
-        printf("%c", array[j]);
-    }
-    printf("\n");
-}
-
-void resetarray(char * array, int length)
-{
-    for(int i = 0; i < length; i ++)
-    {
-        array[i] = ' ';
-    }
-    
 }
 
 struct Node* checkExist(struct Node *head, char *name)
@@ -72,6 +143,11 @@ struct Node* checkExist(struct Node *head, char *name)
         }
         current = current->next;
     }
+    if(strcmp(current->name, name) == 0)
+    {
+        current->count++;
+        return head;
+    }
     struct Node *newNode;
     newNode = LLnode(name, 1, NULL);
     current->next = newNode;
@@ -79,7 +155,7 @@ struct Node* checkExist(struct Node *head, char *name)
 }
 
 struct Node* updateList(struct Node *head, char *name)
-{
+{   
     struct Node *newNode;
     newNode = LLnode(name, 1, NULL);
     if(strcmp(head->name, "!DeFaUt!") == 0)
@@ -98,31 +174,32 @@ int main(int argc, char *argv[])
 {
      struct Node *head;
      head = LLnode("!DeFaUt!", 0, NULL);
-     resetarray(buffer, 20000);
-     resetarray(load, 15);
+     resetarray(buffer, 600000);
+     resetarray(load, 200);
      int fd = open(argv[1], O_RDONLY);
-     read(fd, buffer, 20000);
+     read(fd, buffer, 600000);
      int count = 0;
-     for(int k = 0; k < 20000; k ++)
+
+     for(int k = 0; k < 600000; k ++)
      {
 
         if(buffer[k] != ' ')// right now is not the space
         {
             if((buffer[k] >= 'a' && buffer[k] <= 'z') || (buffer[k] >= 'A' && buffer[k] <= 'Z'))
             {
-                if(*(load+count) == ' ')
+                if(load[count] == ' ')
                 {
-                    *(load + count) = convert(buffer[k]);
+                    load[count] = convertToSmall(buffer[k]);
                     count ++;
                  }
             }
         }
         else// right now is the space,代表要么一个单词结束了， 或者还未开始是前面的空格
         {
-            if(*load != ' ')
+            if(load[0] != ' ')
             {
                 head = updateList(head, load);
-                resetarray(load, 15);
+                resetarray(load, 200);
                 count = 0;
             }
         }
